64b989a9f5d0be3d361e7849d5949c2b33c6fb3b
PavanKaringu <pavankaringu06042000@gmail.com>
2024-12-02 21:01:33 +0530

    Initial commit

---
+++ src/appProvider/AppProvider.jsx
@@ -0,0 +1,17 @@
+// import React, { createContext, useState, useContext } from "react";
+
+// const AppContext = createContext();
+
+// export function AppProvider({ children }) {
+//   const [users, setUsers] = useState([]);
+//   const [roles, setRoles] = useState([]);
+//   return (
+//     <AppContext.Provider value={{ users, setUsers, roles, setRoles }}>
+//       {children}
+//     </AppContext.Provider>
+//   );
+// }
+
+// export function useAppContext() {
+//   return useContext(AppContext);
+// }
